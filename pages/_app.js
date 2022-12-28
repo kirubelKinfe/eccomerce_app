82f27a6e01e570a44613df8ae076e0136c3e40ca
kirubel kinfe <kirubelkinfe01@gmail.com>
2022-12-28 13:11:24 +0300

    First Commit

---
+++ pages/_app.js
@@ -0,0 +1,18 @@
+import '../styles/globals.css'
+import React from 'react'
+import { StateContext } from '../context/StateContext'
+import { Toaster } from 'react-hot-toast'
+import { Layout } from '../components'
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <StateContext>
+      <Layout>
+        <Toaster />
+        <Component {...pageProps} />
+      </Layout>
+    </StateContext>
+  )
+}
+
+export default MyApp
